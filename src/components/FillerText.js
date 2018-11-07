5b1b66014de87a6ca68f98001240976f7ead44d9
Kirby Campbell <jkirbycampbell@gmail.com>
2018-11-07 19:32:23 +0000

    automatically backed up by learn

---
+++ src/components/FillerText.js
@@ -0,0 +1,10 @@
+class RegistrationForm extends Component {
+  render() {
+    return (
+       <form></form>
+       <input type="text"></input>
+       <input type="password"></input>
+       <button type="submit"></button>
+     )
+  }
+}
